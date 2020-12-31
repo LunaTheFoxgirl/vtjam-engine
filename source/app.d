b193d9c924af66289badc6ae6c0a380a739609cf
Member1221 <cli-luna@protonmail.com>
2020-12-31 06:22:36 +0100

    yeeted things I used to test with

--- source/app.d
+++ source/app.d
@@ -3,50 +3,11 @@ import engine;
 import std.stdio;
 import config;
 
-class Ritta {
-private:
-    Texture texture;
-    vec2 position;
-
-public:
-    this() {
-        texture = new Texture("assets/ritta.png");
-        texture.setFiltering(Filtering.Point);
-    }
-
-    void update() {
-        auto keyboardState = Keyboard.getState();
-
-        if (keyboardState.isKeyDown(Key.KeyW)) {
-            position.y -= 4;
-        } else if (keyboardState.isKeyDown(Key.KeyS)) {
-            position.y += 4;
-        }
-
-        if (keyboardState.isKeyDown(Key.KeyA)) {
-            position.x -= 4;
-        } else if (keyboardState.isKeyDown(Key.KeyD)) {
-            position.x += 4;
-        }
-        auto texCenter = texture.center();
-        
-
-        position.x = clamp(position.x, texCenter.x, PlayfieldWidth-texCenter.x);
-        position.y = clamp(position.y, texCenter.y, PlayfieldHeight-texCenter.y);
-
-        GameBatch.draw(texture, vec4(position.x, position.y, texture.width, texture.height), vec4.init, texCenter);
-        GameBatch.flush();
-    }
-}
-
-Ritta ritta;
 void _init() {
-    ritta = new Ritta;
 }
 
 void _update() {
     kmClearColor(vec4(0, 0, 0, 1));
-    ritta.update();
 }
 
 void _cleanup() {
